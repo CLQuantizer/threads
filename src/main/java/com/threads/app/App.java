83eab926fdd9aa058c13017bd2be3307a8ad76ff
CLQuantizer <ezioxxk@gmail.com>
2022-03-28 22:32:13 +0100

    whatever bro, this is on bubble sort

--- src/main/java/com/threads/app/App.java
+++ src/main/java/com/threads/app/App.java
@@ -3,17 +3,16 @@ import java.util.concurrent.*;
 import java.util.*;
 import java.util.stream.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int[]x ={1,4,5,66,7,8};
+        bubbleSort(x);
+        for(int i=0;i<x.length;i++)System.out.println(x[i]);
     }
-    void bubbleSort(int arr[])
+    static void bubbleSort(int arr[])
     {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
